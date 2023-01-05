7b800607e75d63e91136435c92896af4a92ef6b5
Mutenroshi <muten.42@gmail.com>
2023-01-05 17:51:25 +0100

    Update API_Zscaler.py

--- API_Zscaler.py
+++ API_Zscaler.py
@@ -2,111 +2,105 @@ import csv
 import requests
 import time
 
-# Obfuscate the API key
-def obfuscate_api_key(api_key):
-    now = int(time.time() * 1000)
-    n = str(now)[-6:]
-    r = str(int(n) >> 1).zfill(6)
-    key = ""
-    for i in range(0, len(str(n)), 1):
-        key += api_key[int(str(n)[i])]
-    for j in range(0, len(str(r)), 1):
-        key += api_key[int(str(r)[j])+2]
-    return key
+# Set the input and output filenames and the Zscaler info filename
+input_filename = 'URLS.txt'
+output_filename = 'output.csv'
+zscaler_filename = 'zscaler-infos.txt'
 
-# Read the information from the text file
-with open('zscaler-infos.txt', 'r') as file:
+# Read the input file
+with open(input_filename, 'r') as file:
+    # Read the lines in the file
     lines = file.readlines()
 
-# Extract the information from the lines
-host = None
-login = None
-password = None
-api_key = None
-for line in lines:
-    if line.startswith('Host : '):
-        host = line[7:].strip()
-    elif line.startswith('Login : '):
-        login = line[8:].strip()
-    elif line.startswith('Password : '):
-        password = line[11:].strip()
-    elif line.startswith('API Key : '):
-        api_key = line[10:].strip()
+# Remove the newline characters from the lines
+lines = [line.strip() for line in lines]
 
-# Check that all the information was found
-if host is None or login is None or password is None or api_key is None:
-    print("Les informations du fichier zscaler-infos.txt sont incomplètes")
-    exit()
+# Split the lines into chunks of 100 URLs
+url_chunks = [lines[i:i+100] for i in range(0, len(lines), 100)]
 
-# Obfuscate the API key
-obfuscated_api_key = obfuscate_api_key(api_key)
+# Load the Zscaler info from the file
+with open(zscaler_filename, 'r') as file:
+    # Read the lines in the file
+    lines = file.readlines()
 
-# Authenticate the user
+# Extract the host, login, password, role and API key from the lines
+host = lines[1].split(': ')[1].strip()
+login = lines[2].split(': ')[1].strip()
+password = lines[3].split(': ')[1].strip()
+role = lines[4].split(': ')[1].strip()
+api_key = lines[5].split(': ')[1].strip()
+
+# Obfuscate the API key with the current timestamp
+timestamp = str(int(time.time() * 1000))
+n = timestamp[-6:]
+r = str(int(n) >> 1).zfill(6)
+obfuscated_key = ""
+for i in range(0, len(n), 1):
+    obfuscated_key += api_key[int(n[i])]
+for j in range(0, len(r), 1):
+    obfuscated_key += api_key[int(r[j])+2]
+
+# Authenticate with the API
 headers = {
     'Content-Type': 'application/json',
     'Cache-Control': 'no-cache'
 }
 data = {
-    'apiKey': obfuscated_api_key,
+    'apiKey': obfuscated_key,
     'username': login,
     'password': password,
-    'timestamp': str(int(time.time() * 1000))
+    'timestamp': timestamp
 }
 r = requests.post(f'https://{host}/api/v1/authenticatedSession', headers=headers, json=data)
 
-# Check that the authentication was successful
+# Check that the request was successful
 if r.status_code != 200:
     print(f"Erreur {r.status_code} lors de l'authentification")
     exit()
 
-# Get the JSESSIONID from the response
+# Extract the JSESSIONID from the response
 jsessionid = r.cookies['JSESSIONID']
-
-# Open the text file with the URLs and read each line
-with open('URLS.txt', 'r') as file:
-    lines = file.readlines()
 
 # Initialize the list of results
 results = []
 
-# For each group of 100 URLs
-for i in range(0, len(lines), 100):
-    # Create the list of URLs for this group
-    url_list = lines[i:i+100]
-
+# Call the Zscaler API for each chunk of URLs
+for url_list in url_chunks:
     # Call the Zscaler API with the list of URLs
     headers = {
         'Content-Type': 'application/json',
-        'Cache-Control': 'no-cache',
-        'Cookie': f'JSESSIONID={jsessionid}'
-    }
-    data = url_list
-    r = requests.post(f'https://{host}/api/v1/urlLookup', headers=headers, json=data)
+        'Cache-Control': 'no-cache'
+}
+data = {
+    'urls': url_list
+}
+r = requests.post(f'https://{host}/api/v1/urlLookup', headers=headers, json=data, cookies={'JSESSIONID': jsessionid})
 
-    # Check that the request was successful
-    if r.status_code != 200:
-        print(f"Erreur {r.status_code} lors de l'appel de l'API")
-        exit()
+# Check that the request was successful
+if r.status_code != 200:
+    print(f"Erreur {r.status_code} lors de l'appel de l'API")
+    exit()
 
-    # Add the results to the list
-    results += r.json()
+# Extract the URL classification information from the response
+url_classification_data = r.json()
 
-# Open the output CSV file
-with open('output.csv', 'w', newline='') as file:
-    # Create the CSV writer
-    writer = csv.writer(file, delimiter=';')
+# Add the URL classification information to the results
+for item in url_classification_data:
+    url = item['url']
+    categories = ';'.join(item['urlClassifications'])
+    results.append((url, categories))
 
+# Write the results to the output file
+with open(output_filename, 'w') as file:
     # Write the header row
-    writer.writerow(['Numéro', 'URL', 'Catégories'])
-
-    # Write the rows with the results
+    file.write('Numéro;URL;Catégories\n')
+    # Write the data rows
     for i, result in enumerate(results):
-        categories = ','.join(result['urlClassifications'])
-        writer.writerow([i+1, result['url'], categories])
+        file.write(f"{i+1};{result[0]};{result[1]}\n")
 
 # Log out of the API
-headers = {
-    'Cache-Control': 'no-cache',
-    'Cookie': f'JSESSIONID={jsessionid}'
-}
-requests.delete(f'https://{host}/api/v1/authenticatedSession', headers=headers)
+r = requests.delete(f'https://{host}/api/v1/authenticatedSession', cookies={'JSESSIONID': jsessionid})
+
+# Check that the request was successful
+if r.status_code != 200:
+    print(f"Erreur {r.status_code} lors de la déconnexion")
